ad32335ecc08830beb731ddd63b641a7bc0ca0d2
Denis <dalekseenko8@gmail.com>
2025-10-18 00:08:08 +0300

    create something

--- models/models.py
+++ models/models.py
@@ -1,7 +1,8 @@
 from pydantic import BaseModel
+from datetime import datetime
 
 class RequestData(BaseModel):
     amount: int
     to: str
     firstly: str
-    date: str
+    date: str = datetime.now()
