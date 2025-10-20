1282d9ebf6a4b5d449617bf2d05a5b76e327d6b7
Denis <dalekseenko8@gmail.com>
2025-10-20 20:07:14 +0300

    create redis notification

---
+++ notifications/connect_tgbot.py
@@ -0,0 +1,28 @@
+from telebot import TeleBot
+from dotenv import load_dotenv
+import requests
+import os
+
+load_dotenv()
+
+# 578814803
+
+ADMINS = ['1108856135']
+
+class Bot(object):
+    def __init__(self):
+        self.BOT_TOKEN = os.getenv("TOKEN_BOT")
+        self.bot = TeleBot(token=self.BOT_TOKEN)
+        self.url = f"https://api.telegram.org/bot{self.BOT_TOKEN}/sendMessage"
+        
+    def _send_message(self, text: str = "Подозрительная транзакция"):
+        for admin_id in ADMINS:
+            payload = {"chat_id": admin_id, "text": text, "parse_mode": "HTML"}
+            try:
+                response = requests.post(self.url, json=payload, timeout=5)
+            except:
+                print("Не удалось")
+        
+        
+bot = Bot()
+bot._send_message()
