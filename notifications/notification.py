d14318794dc7c5c491c7634de4ef3be947e98f3d
Denis <dalekseenko8@gmail.com>
2025-10-20 20:07:14 +0300

    create redis notification

---
+++ notifications/notification.py
@@ -0,0 +1,58 @@
+import redis
+from dotenv import load_dotenv
+from connect_tgbot import Bot
+import os
+import time
+import json
+
+load_dotenv()
+
+REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+
+class Redis(object):
+    def __init__(self):
+        self.redis = redis.Redis.from_url(REDIS_URL, decode_responses=True)
+        self.bot = Bot()
+        
+    def listener(self):
+        last_id = "0" 
+
+        while True:
+            try:
+                messages = self.redis.xread({"alerts_stream": last_id}, block=0)
+                for stream, entries in messages:
+                    for message_id, data in entries:
+                        last_id = message_id
+                        try:
+                            data["severity"] = float(data["severity"])
+                        except Exception:
+                            pass
+
+                        print(f"📥 Получено сообщение из Redis: {data}")
+
+                        text = (
+                            f"🚨 <b>Подозрительная активность</b>\n"
+                            f"ID: <code>{data['id']}</code>\n"
+                            f"Описание: {data['details']}\n"
+                            f"Уровень риска: {data['severity']:.2f}"
+                        )
+                        self.bot._send_message(text)
+            except Exception as e:
+                print(f"❌ Ошибка при чтении из Redis Streams: {e}")
+                time.sleep(2)
+                    
+    def send_alert(self, id, details, severity):
+        """Публикует новое уведомление в Redis Stream."""
+        message = {
+            "id": str(id),
+            "details": details,
+            "severity": str(severity)
+        }
+        self.redis.xadd("alerts_stream", message)
+        print(f"📤 Отправлено в Redis Stream: {message}")
+
+notificator = Redis()
+                   
+notificator.send_alert(123, "Попытка фальсификации данных", 0.91)
+
+notificator.listener()
