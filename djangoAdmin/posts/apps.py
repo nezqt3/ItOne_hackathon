098da027429eb0bdc22ad7b8c444382b0d7ee4a9
Denis <dalekseenko8@gmail.com>
2025-10-21 09:19:37 +0300

    add tables on admin page

--- djangoAdmin/posts/apps.py
+++ djangoAdmin/posts/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
+class TransactionsConfig(AppConfig):
+    default_auto_field = "django.db.models.Transactions"
+    name = "transactions"
 
-class PostsConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "posts"
+class TransactionsTypesConfig(AppConfig):
+    default_auto_field = "django.db.models.TransactionsTypes"
+    name = "transactions_types"
--- djangoAdmin/posts/models.py
+++ djangoAdmin/posts/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class Transactions(models.Model):
-    transaction_id = models.IntegerField(primary_key=True)
+    transaction_id = models.IntegerField(primary_key=True, unique=True)
     correlation_id = models.IntegerField()
     timestamp = models.DateTimeField()
     sender_account = models.CharField(max_length=255)
     receiver_account = models.CharField(max_length=255)
-    amount = models.IntegerField(max_length=30)
+    amount = models.IntegerField()
     transaction_type = models.CharField(max_length=30)
     merchant_category = models.CharField(max_length=30)
     location = models.CharField(max_length=30)
