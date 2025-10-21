052df777d64df569adfa402b45fef02c72961f78
Denis <dalekseenko8@gmail.com>
2025-10-21 09:02:31 +0300

    create tables transactions and transactions_types

--- djangoAdmin/posts/models.py
+++ djangoAdmin/posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+class Transactions(models.Model):
+    transaction_id = models.IntegerField(primary_key=True)
+    correlation_id = models.IntegerField()
+    timestamp = models.DateTimeField()
+    sender_account = models.CharField(max_length=255)
+    receiver_account = models.CharField(max_length=255)
+    amount = models.IntegerField(max_length=30)
+    transaction_type = models.CharField(max_length=30)
+    merchant_category = models.CharField(max_length=30)
+    location = models.CharField(max_length=30)
 
+class TransactionsTypes(models.Model):
+    transaction_id = models.IntegerField(primary_key=True)
+    transaction_type = models.CharField(max_length=30)
